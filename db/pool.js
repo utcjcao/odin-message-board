913c13ee3275d775db5b04e5bec98d33896cb188
utcjcao <chrisj.cao@mail.utoronto.ca>
2025-01-02 01:13:25 -0500

    Update pool.js

--- db/pool.js
+++ db/pool.js
@@ -2,6 +2,7 @@ const { Pool } = require("pg");
 require("dotenv").config("../.env");
 
 const connectionString = process.env.DATABASE_URL;
+console.log(connectionString);
 
 module.exports = new Pool({
   connectionString: connectionString,
